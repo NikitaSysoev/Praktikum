a4983d1f8e7175eba50b4a7755d9a5a691218985
nikita <na.sysoev@gmail.com>
2020-10-05 16:35:28 +0300

    fixed I

--- sprint2-part2/I.js
+++ sprint2-part2/I.js
@@ -2,35 +2,61 @@ const fs = require('fs');
 const os = require('os');
 const input = fs.readFileSync('input.txt', 'utf-8').split(os.EOL);
 
-const winners = Number(input[0]);
-let nums = input[2].split(' ').map(Number);
+let winners = Number(input[0]);
+const nums = input[2].split(' ').map(Number);
 
+const sum = nums.reduce((acc, item) => acc + item, 0);
 
-
-
-const sum = (arr) => arr.reduce((acc, item) => acc + item, 0);
-
-// if(sum % winners !== 0){
-//     console.log('False')
-//     return;
-// }
+if (sum % winners !== 0) {
+  console.log('False');
+  return;
+}
 
 const priz = sum / winners;
 
-nums = nums.sort((a,b)=>{
-    if(a>b){
-        return -1;
-    }
-    if(a<b){
-        return 1;
-    }
-    return 0;
-});
+if (nums.find((num) => num > priz)) {
+  console.log('False');
+  return;
+}
 
-const result = [...nums];
+if (nums.length < winners) {
+  console.log('False');
+  return;
+}
 
-for(let i=0; i<nums.length; i++){
-    if(nums[i] === priz && sum(nums.slice(1)) % winners ===0){
-        
+const x = nums.filter((item) => item === priz);
+let j = x.length;
+while (j > 0) {
+  let n = nums.find((item) => item === priz);
+  const index = nums.indexOf(n);
+  nums.splice(index, 1);
+  j--;
+  winners--;
+}
+
+nums.sort((a, b) => b - a);
+
+const wins = Array(winners).fill(0);
+
+function search(arr) {
+  if (arr.length === 0) {
+    return;
+  }
+  const moneta = arr.shift();
+  for (let i = 0; i < wins.length; i++) {
+    if (wins[i] + moneta <= priz) {
+      wins[i] += moneta;
+    } else {
+      continue;
     }
+  }
+  return search(arr);
+}
+
+search(nums);
+
+if (wins.every((item) => item === priz)) {
+  console.log('True');
+} else {
+  console.log('False');
 }

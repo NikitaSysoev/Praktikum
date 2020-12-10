ae210c0d1444e85fc3420bb9eebb2ebafa164c5d
nikita <na.sysoev@gmail.com>
2020-12-10 20:26:26 +0300

    resolved print 5

--- final5/B.js
+++ final5/B.js
@@ -2,29 +2,82 @@ const fs = require('fs');
 const os = require('os');
 const input = fs.readFileSync('input.txt', 'utf-8').split(os.EOL);
 
-function HashTable() {
-  const store = [];
+class Node {
+  constructor(key, value) {
+    this.key = key;
+    this.value = value;
+    this.next = null;
+  }
+}
 
-  this.get = (key) => {
-    const result = store[key];
-    if (result == null) {
-      return -1;
+class HashTable {
+  constructor() {
+    this.table = new Array(1000).fill(null);
+  }
+
+  calcHash(key) {
+    return (key % 997) + 1;
+  }
+
+  find(key, node) {
+    while (node) {
+      if (node.key === key) {
+        return node;
+      }
+      node = node.next;
     }
-    return result;
-  };
+    return null;
+  }
 
-  this.put = (key, value) => {
-    store[key] = value;
-  };
+  put(key, value) {
+    const index = this.calcHash(key);
 
-  this.delete = (key) => {
-    const result = store[key];
-    if (result == null) {
+    if (this.table[index] !== null) {
+      const parent = this.table[index];
+      let node = this.find(key, parent);
+      if (node === null) {
+        node = new Node(key, value);
+        node.next = parent;
+        this.table[index] = node;
+      } else {
+        node.value = value;
+      }
+    } else {
+      this.table[index] = new Node(key, value);
+    }
+  }
+
+  delete(key) {
+    let index = this.calcHash(key);
+    if (this.table[index] === null) {
       return 'error';
     }
-    delete store[key];
-    return 'ok';
-  };
+    let node = this.table[index];
+    let prev = null;
+    while (node) {
+      if (node.key === key) {
+        if (prev) {
+          prev.next = node.next;
+        } else {
+          this.table[index] = node.next;
+        }
+        return 'ok';
+      }
+      prev = node;
+      node = node.next;
+    }
+    return 'error';
+  }
+
+  get(key) {
+    let index = this.calcHash(key);
+    const node = this.find(key, this.table[index]);
+    if (node) {
+      return node.value;
+    } else {
+      return -1;
+    }
+  }
 }
 
 const hashTable = new HashTable();
@@ -44,3 +97,5 @@ for (let i = 1; i < input.length; i++) {
       break;
   }
 }
+
+// 44646459
